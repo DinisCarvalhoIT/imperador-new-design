802860c91c95fd9400e8b17bcba04ba707b07d63
filipe942 <josefolope@gmail.com>
2025-11-07 17:01:24 +0000

    style : footer done

--- src/components/Footer/FormFooter.tsx
+++ src/components/Footer/FormFooter.tsx
@@ -14,20 +14,27 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { ui } from "../../i18n/ui";
 
-const formSchema = z.object({
-  name: z
-    .string()
-    .min(1, "Name is required")
-    .max(50, "Name must be less than 50 characters"),
-  email: z.string().email("Invalid email address"),
-  message: z
-    .string()
-    .min(1, "Message is required")
-    .max(500, "Message must be less than 500 characters"),
-});
+type Props = {
+  lang: keyof typeof ui;
+};
 
-export default function FormFooter() {
+export default function FormFooter({ lang }: Props) {
+  const t = ui[lang];
+
+  const formSchema = z.object({
+    name: z
+      .string()
+      .min(1, t["footer.form.validation.name_required"])
+      .max(50, t["footer.form.validation.name_max"]),
+    email: z.string().email(t["footer.form.validation.email_invalid"]),
+    message: z
+      .string()
+      .min(1, t["footer.form.validation.message_required"])
+      .max(500, t["footer.form.validation.message_max"]),
+  });
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -42,17 +49,20 @@ export default function FormFooter() {
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="">
-        <div className="grid grid-cols-2 w-full items-center justify-items-center gap-x-3 pb-3">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full 2xl:max-w-[70%] lg:max-w-full md:max-w-[60%] sm:max-w-[80%] max-w-[90%]"
+      >
+        <div className="grid lg:grid-cols-2 grid-cols-1 w-full items-center gap-x-3 pb-3">
           <FormField
             control={form.control}
             name="name"
             render={({ field }) => (
-              <FormItem>
+              <FormItem className="w-full">
                 <FormControl>
                   <Input
-                    className="bg-white/84 border border-[#a3a3a0] text-sm text-black font-montserrat tracking-widest font-light"
-                    placeholder="Nome"
+                    className="bg-white/84 border border-[#a3a3a0] text-sm text-black font-montserrat tracking-widest font-light lg:mb-0 mb-3 w-full"
+                    placeholder={t["footer.form.name_placeholder"]}
                     {...field}
                   />
                 </FormControl>
@@ -64,12 +74,12 @@ export default function FormFooter() {
             control={form.control}
             name="email"
             render={({ field }) => (
-              <FormItem>
+              <FormItem className="w-full">
                 <FormControl>
                   <Input
-                    className="bg-white/84 border border-[#a3a3a0] text-sm text-black font-montserrat tracking-widest font-light"
+                    className="bg-white/84 border border-[#a3a3a0] text-sm text-black font-montserrat tracking-widest font-light w-full"
                     type="email"
-                    placeholder={"Email"}
+                    placeholder={t["footer.form.email_placeholder"]}
                     {...field}
                   />
                 </FormControl>
@@ -83,11 +93,11 @@ export default function FormFooter() {
           control={form.control}
           name="message"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="w-full">
               <FormControl>
                 <Textarea
-                  className="bg-white/84 border border-[#a3a3a0] h-[90px] text-sm sm:text-base text-black font-montserrat tracking-widest font-light resize-none"
-                  placeholder={"Mensagem"}
+                  className="bg-white/84 border border-[#a3a3a0] h-[90px] text-sm text-black font-montserrat tracking-widest font-light resize-none w-full"
+                  placeholder={t["footer.form.message_placeholder"]}
                   {...field}
                 />
               </FormControl>
@@ -101,7 +111,7 @@ export default function FormFooter() {
             className="text-white text-base bg-[#7192A2] hover:bg-[#7192A2]/80 hover:border-2 w-full font-montserrat font-semibold cursor-pointer"
             type="submit"
           >
-            {"Agendar Visita"}
+            {t["footer.form.submit_button"]}
           </Button>
         </div>
       </form>
