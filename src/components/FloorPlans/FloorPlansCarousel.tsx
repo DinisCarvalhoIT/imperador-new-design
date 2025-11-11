74f3deb2335e893ffa1daddc3b3cf21fe96049d5
filipe942 <josefolope@gmail.com>
2025-11-11 12:17:18 +0000

    style : added all sections needed for T1 page

--- src/components/FloorPlans/FloorPlansCarousel.tsx
+++ src/components/FloorPlans/FloorPlansCarousel.tsx
@@ -10,7 +10,7 @@ import {
 import { Card, CardContent } from "@/components/ui/card";
 import { ui } from "@/i18n/ui";
 
-type DataCarousel = {
+export type DataCarousel = {
   id: string;
   floorPlanImage: string;
   smallImageCollumnTable: boolean;
@@ -25,110 +25,14 @@ type DataCarousel = {
 
 type FloorPlansCarouselProps = {
   lang: keyof typeof ui;
+  dataCarousel: DataCarousel[];
 };
 
-const dataCarousel: DataCarousel[] = [
-  {
-    id: "floor-plan-1",
-    floorPlanImage: "/FloorPlans/T1_Modelo_A.png",
-    smallImageCollumnTable: true,
-    smallImageStyle:
-      "hidden lg:flex w-[100px] h-[52px] sm:w-[120px] sm:h-[62px] md:w-[140px] md:h-[73px] lg:w-[171px] lg:h-[89px]",
-    smallFloorPlanImage: "/FloorPlans/T1_Modelo_A_Small.svg",
-    tableImage: "/FloorPlans/T1_Modelo_A_Table.svg",
-    tableImageStyle:
-      "w-full max-w-[250px] sm:max-w-[300px] md:max-w-[350px] lg:max-w-[400px] h-full max-h-[250px] sm:max-h-[300px] md:max-h-[400px] lg:max-h-[360px] flex items-center justify-center",
-    mainTitleKey: "floorplans.t1_model_a",
-    mobileTitleKey: "floorplans.t1_model_a_mobile",
-    subTitleKey: "floorplans.t1_model_a_subtitle",
-  },
-  {
-    id: "floor-plan-2",
-    floorPlanImage: "/FloorPlans/T1_Modelo_B.png",
-    smallImageCollumnTable: false,
-    smallImageStyle:
-      "hidden lg:flex w-[100px] h-[52px] sm:w-[120px] sm:h-[62px] md:w-[140px] md:h-[73px] lg:w-[171px] lg:h-[89px] mx-auto",
-    smallFloorPlanImage: "/FloorPlans/T1_Modelo_B_Small.svg",
-    tableImage: "/FloorPlans/T1_Modelo_B_Table.svg",
-    tableImageStyle:
-      "w-full max-w-[250px] sm:max-w-[300px] md:max-w-[350px] lg:max-w-[400px] h-full max-h-[270px] sm:max-h-[300px] md:max-h-[400px] lg:max-h-[360px] flex items-center justify-center",
-    mainTitleKey: "floorplans.t1_model_b",
-    mobileTitleKey: "floorplans.t1_model_b_mobile",
-    subTitleKey: "floorplans.t1_model_b_subtitle",
-  },
-  {
-    id: "floor-plan-3",
-    floorPlanImage: "/FloorPlans/T1_Modelo_C.png",
-    smallImageCollumnTable: false,
-    smallImageStyle:
-      "hidden lg:flex w-[100px] h-[52px] sm:w-[120px] sm:h-[62px] md:w-[140px] md:h-[73px] lg:w-[171px] lg:h-[89px] mx-auto",
-    smallFloorPlanImage: "/FloorPlans/T1_Modelo_C_Small.svg",
-    tableImage: "/FloorPlans/T1_Modelo_C_Table.svg",
-    tableImageStyle:
-      "w-full max-w-[250px] sm:max-w-[300px] md:max-w-[350px] lg:max-w-[400px] h-full max-h-[250px] sm:max-h-[300px] md:max-h-[400px] lg:max-h-[360px] flex items-center justify-center",
-    mainTitleKey: "floorplans.t1_model_c",
-    mobileTitleKey: "floorplans.t1_model_c_mobile",
-    subTitleKey: "floorplans.t1_model_c_subtitle",
-  },
-  {
-    id: "floor-plan-4",
-    floorPlanImage: "/FloorPlans/T2_Modelo_A.png",
-    smallImageCollumnTable: false,
-    smallImageStyle:
-      "hidden lg:flex absolute bottom-50 xl:bottom-52 -right-11 xl:-right-5 w-[100px] h-[52px] sm:w-[120px] sm:h-[62px] md:w-[140px] md:h-[73px] lg:w-[171px] lg:h-[89px]",
-    smallFloorPlanImage: "/FloorPlans/T2_Modelo_A_Small.svg",
-    tableImage: "/FloorPlans/T2_Modelo_A_Table.svg",
-    tableImageStyle:
-      "w-full max-w-[290px] sm:max-w-[330px] md:max-w-[360px] lg:max-w-[400px] h-full max-h-[330px] sm:max-h-[360px] md:max-h-[450px] lg:max-h-[450px] flex items-center justify-center",
-    mainTitleKey: "floorplans.t2_model_a",
-    mobileTitleKey: "floorplans.t2_model_a_mobile",
-    subTitleKey: "floorplans.t2_model_a_subtitle",
-  },
-  {
-    id: "floor-plan-5",
-    floorPlanImage: "/FloorPlans/T2_Modelo_B.png",
-    smallImageCollumnTable: false,
-    smallImageStyle:
-      " hidden lg:flex absolute bottom-50 xl:bottom-53 -left-2 xl:-left-4 w-[100px] h-[52px] sm:w-[120px] sm:h-[62px] md:w-[140px] md:h-[73px] xl:w-[171px] xl:h-[89px] lg:w-[141px] lg:h-[89px]",
-    smallFloorPlanImage: "/FloorPlans/T2_Modelo_B_Small.svg",
-    tableImage: "/FloorPlans/T2_Modelo_B_Table.svg",
-    tableImageStyle:
-      "w-full max-w-[290px] sm:max-w-[330px] md:max-w-[360px] lg:max-w-[400px] h-full max-h-[330px] sm:max-h-[360px] md:max-h-[450px] lg:max-h-[450px] flex items-center justify-center",
-    mainTitleKey: "floorplans.t2_model_b",
-    mobileTitleKey: "floorplans.t2_model_b_mobile",
-    subTitleKey: "floorplans.t2_model_b_subtitle",
-  },
-  {
-    id: "floor-plan-6",
-    floorPlanImage: "/FloorPlans/T3_Modelo_A.png",
-    smallImageCollumnTable: false,
-    smallImageStyle:
-      "hidden lg:flex absolute bottom-100 xl:bottom-110 -right-17 xl:-right-7 w-[100px] h-[52px] sm:w-[120px] sm:h-[62px] md:w-[140px] md:h-[73px] lg:w-[171px] lg:h-[89px]",
-    smallFloorPlanImage: "/FloorPlans/T3_Modelo_A_Small.svg",
-    tableImage: "/FloorPlans/T3_Modelo_A_Table.svg",
-    tableImageStyle:
-      "w-full max-w-[290px] sm:max-w-[360px] md:max-w-[380px] lg:max-w-[400px] h-full max-h-[370px] sm:max-h-[400px] md:max-h-[480px] lg:max-h-[450px] flex items-center justify-center",
-    mainTitleKey: "floorplans.t3_model_a",
-    mobileTitleKey: "floorplans.t3_model_a_mobile",
-    subTitleKey: "floorplans.t3_model_a_subtitle",
-  },
-  {
-    id: "floor-plan-7",
-    floorPlanImage: "/FloorPlans/T3_Modelo_B.png",
-    smallImageCollumnTable: false,
-    smallImageStyle:
-      " hidden lg:flex absolute bottom-110 xl:bottom-122 -left-2 xl:-left-3 w-[100px] h-[52px] sm:w-[120px] sm:h-[62px] md:w-[140px] md:h-[73px] lg:w-[100px] lg:h-[89px] xl:w-[150px] xl:h-[89px]",
-    smallFloorPlanImage: "/FloorPlans/T3_Modelo_B_Small.svg",
-    tableImage: "/FloorPlans/T3_Modelo_B_Table.svg",
-    tableImageStyle:
-      "w-full max-w-[290px] sm:max-w-[360px] md:max-w-[380px] lg:max-w-[400px] h-full max-h-[370px] sm:max-h-[400px] md:max-h-[480px] lg:max-h-[450px] flex items-center justify-center",
-    mainTitleKey: "floorplans.t3_model_b",
-    mobileTitleKey: "floorplans.t3_model_b_mobile",
-    subTitleKey: "floorplans.t3_model_b_subtitle",
-  },
-];
 // absolute inset-0 ---- no primeiro div itnha
-export default function FloorPlansCarousel({ lang }: FloorPlansCarouselProps) {
+export default function FloorPlansCarousel({
+  lang,
+  dataCarousel,
+}: FloorPlansCarouselProps) {
   const [api, setApi] = useState<CarouselApi>();
   const [current, setCurrent] = useState(0);
 
