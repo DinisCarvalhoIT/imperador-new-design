d69cecd521e3a4837062a29d09e3819a2bd15a99
Mictilt <jvmiguelito235@gmail.com>
2025-11-11 17:59:28 +0000

    feat: added favicon, metadata, robots.txt, sitemap and sitelinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,14 @@ import react from '@astrojs/react';
 
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 const compilerConfig = { 
   target: '19' // can be '17' | '18' | '19', default is 19
 };
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://imperador-building.com',
   vite: {
     plugins: [tailwindcss()],
     ssr: {
@@ -22,13 +25,24 @@ export default defineConfig({
     }
   },
 
-  integrations: [react({
-    babel: {
-      plugins: [
-        // make sure this is first in the plugins list
-        ["babel-plugin-react-compiler", compilerConfig] 
-      ]
-    }
-  })],
+  integrations: [
+    react({
+      babel: {
+        plugins: [
+          // make sure this is first in the plugins list
+          ["babel-plugin-react-compiler", compilerConfig] 
+        ]
+      }
+    }),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          pt: 'pt',
+        },
+      },
+    })
+  ],
   adapter: vercel()
 });
